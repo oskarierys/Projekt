d3e130360ab4424253d9140cb87abd5b084d15ad
Kierys Oskar <osk.kierys@gmail.com>
2024-07-15 20:56:09 +0200

    Add project files.

---
+++ Projekt/Models/Person.cs
@@ -0,0 +1,10 @@
+﻿namespace Projekt.Models
+{
+    internal class Person
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int Age { get; set; }
+        public string Gender { get; set; }
+	}
+}
---
+++ Projekt/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,44 @@
+﻿using Projekt.Models;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using Projekt.MVVM;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Projekt.ViewModels
+{
+    internal class MainWindowViewModel : ViewModelBase
+    {
+        public ObservableCollection<Person> Persons { get; set; }
+
+        public RelayCommand AddCommand => new RelayCommand(execute => AddPerson());
+        public RelayCommand DeleteCommand => new RelayCommand(execute => DeletePerson(), canExecute => SelectedPerson != null);
+
+        public MainWindowViewModel()
+        {
+            Persons = new ObservableCollection<Person>();
+        }
+
+        private Person selectedPerson;
+        public Person SelectedPerson
+        {
+            get { return selectedPerson; }
+            set 
+            { 
+                selectedPerson = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private void AddPerson()
+        {
+            
+        }
+
+        private void DeletePerson()
+        {
+            Persons.Remove(SelectedPerson);
+        }
+
+    }
+}
+    
